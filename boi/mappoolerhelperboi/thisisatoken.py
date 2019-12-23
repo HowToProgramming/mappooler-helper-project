8b22edab4cfe93d41059284ba69b50a2e58cff98
unknown <lkycst@gmail.com>
2019-12-23 15:30:46 +0700

    More secure for myself

--- boi/mappoolerhelperboi/thisisatoken.py
+++ boi/mappoolerhelperboi/thisisatoken.py
@@ -1,1 +1,12 @@
-token = "1c59b8afe9d40ce6ba029453cd62b35d3a005b4c"
+convertedtoken = 161852596426573538808593526471880393572430666572
+def dectohex(num):
+    h = ""
+    while num != 0:
+        r = num % 16
+        if r <= 9:
+            h = str(r) + h
+        else:
+            h = chr(87 + r) + h
+        num //= 16
+    return h
+token = dectohex(convertedtoken)
--- start.py
+++ start.py
@@ -2,8 +2,12 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pyosu import main
 from thisisatoken import token
+import base64
+import json
 osu = main(token)
-
+loginfile = open("agpwejgepigh888887777.otnpj", "rb").read()
+logindata = base64.decodebytes(loginfile)
+logindatareal = json.loads(logindata.decode("ascii"))
 scope = ['https://www.googleapis.com/auth/spreadsheets']
-credentials = ServiceAccountCredentials.from_json_keyfile_name('omdtmappool-d88728cc74b2.json', scope)
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(logindatareal, scope)
 atr = gspread.authorize(credentials)
